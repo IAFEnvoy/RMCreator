eef71baae61e8b6024ffdc8c96efc6a595593648
IAFEnvoy <tanksheng545@126.com>
2026-04-19 10:41:27 +0800

    add app download link

--- js/version.js
+++ js/version.js
@@ -1,4 +1,6 @@
 export const version = '1.0-beta.3';
+// Capability for packed app
+export const isTauri = !!window.__TAURI__ || window.location.protocol === 'tauri:';
 
 export async function loadVersion() {
   let commit = await fetch('/commit.json')
@@ -6,5 +8,8 @@ export async function loadVersion() {
     .then(res => res.commit.substring(0, 7))
     .catch(console.log);
   if (!commit) commit = 'dev';
-  document.getElementById("rmcVersion").innerText = `v${version}-${commit}`;
+  document.getElementById("rmcVersion").innerText = isTauri ? `v${version}` : `v${version}-${commit}`;
+  let downloadAppBtn = document.getElementById('downloadApp');
+  if (isTauri) downloadAppBtn.hidden = true;
+  else downloadAppBtn.onclick = _ => window.open('https://github.com/IAFEnvoy/RMCreator-App/releases', '_blank');
 }
